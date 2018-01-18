1300525eaefa16bb7b3125c3a0ed700f65c2c1c1
Titipat <ttpsvb@gmail.com>
2018-01-18 11:01:32 +0700

    Add a boilerplate code

---
+++ src/Homework1.java
@@ -0,0 +1,17 @@
+
+public class Homework1 {
+
+	public static void main(String[] args) {
+		// Begin of arguments input sample
+		if (args.length > 0) {
+			String input = args[0];
+			if (input.equalsIgnoreCase("251-*32*+")) {
+				System.out.println("(2*(5-1))+(3*2)=14");
+			}
+		}
+		// End of arguments input sample
+		
+		// TODO: Implement your project here
+	}
+
+}
